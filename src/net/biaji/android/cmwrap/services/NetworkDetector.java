1af31bda1bf1a5b928363dcd7356db64e84d9a1d
biaji.cn <biaji.cn@76e3dd0e-3f09-11de-917f-4137a9cc17b9>
2009-05-24 14:13:37 +0000

    加入broadcastreceiver
    git-svn-id: https://cmwrap.googlecode.com/svn/trunk@26 76e3dd0e-3f09-11de-917f-4137a9cc17b9

---
+++ src/net/biaji/android/cmwrap/services/NetworkDetector.java
@@ -0,0 +1,20 @@
+package net.biaji.android.cmwrap.services;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * 此类用于探查网络设置改变并决定隧道应用的开启与否
+ * @author biaji
+ *
+ */
+public class NetworkDetector extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
