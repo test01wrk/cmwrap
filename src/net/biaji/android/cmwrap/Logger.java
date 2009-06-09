2aaa1250285149f83f75095a050f246ca255ccb3
biaji.cn <biaji.cn@76e3dd0e-3f09-11de-917f-4137a9cc17b9>
2009-06-09 14:08:35 +0000

    alpha-4
    git-svn-id: https://cmwrap.googlecode.com/svn/trunk@55 76e3dd0e-3f09-11de-917f-4137a9cc17b9

--- src/net/biaji/android/cmwrap/Logger.java
+++ src/net/biaji/android/cmwrap/Logger.java
@@ -8,27 +8,27 @@ public class Logger {
 	private static int debugLevel = Log.WARN;
 
 	public static void v(String TAG, String msg) {
-		if (debugLevel >= Log.VERBOSE)
-			Log.d(TAG, msg);
+		if (debugLevel <= Log.VERBOSE)
+			Log.v(TAG, msg);
 	}
 
 	public static void d(String TAG, String msg) {
-		if (debugLevel >= Log.DEBUG)
+		if (debugLevel <= Log.DEBUG)
 			Log.d(TAG, msg);
 	}
 
 	public static void i(String TAG, String msg) {
-		if (debugLevel >= Log.INFO)
+		if (debugLevel <= Log.INFO)
 			Log.i(TAG, msg);
 	}
 
 	public static void e(String TAG, String msg) {
-		if (debugLevel >= Log.ERROR)
+		if (debugLevel <= Log.ERROR)
 			Log.e(TAG, msg);
 	}
 
 	public static void e(String TAG, String msg, Throwable e) {
-		if (debugLevel >= Log.ERROR)
+		if (debugLevel <= Log.ERROR)
 			Log.e(TAG, msg, e);
 	}
 
