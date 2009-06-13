559640bbc8dd8c7bf984803a5906d10ba096ad85
biaji.cn <biaji.cn@76e3dd0e-3f09-11de-917f-4137a9cc17b9>
2009-06-13 06:40:07 +0000

    菜单整理完毕，准备加入测试
    git-svn-id: https://cmwrap.googlecode.com/svn/trunk@65 76e3dd0e-3f09-11de-917f-4137a9cc17b9

--- src/net/biaji/android/cmwrap/Cmwrap.java
+++ src/net/biaji/android/cmwrap/Cmwrap.java
@@ -23,9 +23,11 @@ import java.io.IOException;
 
 import net.biaji.android.cmwrap.services.WrapService;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -37,156 +39,11 @@ public class Cmwrap extends Activity implements OnClickListener {
 
 	private TextView logWindow;
 
-	private final String TAG = "CMWRAP->";
-
 	private int serviceLevel = WrapService.SERVER_LEVEL_NULL;
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	private final String TAG = "CMWRAP->";
 
-		super.onCreate(savedInstanceState);
-
-		setContentView(R.layout.main);
-
-		logWindow = (TextView) findViewById(R.id.logwindow);
-
-		// 判断是否需要更新hosts文件
-		if (appStatus() == 1) {
-			logWindow.append("hosts文件不须更新\n");
-		} else {
-
-			logWindow.append("hosts文件更新...\n");
-			int result = Utils.rootCMD(getString(R.string.CMDremount));
-			if (result != 0) {
-				logWindow.append(getString(R.string.ERR_NO_ROOT));
-			} else {
-				installFiles("/system/etc/hosts", R.raw.hosts, null);
-				logWindow.append("更新完毕。\n");
-			}
-		}
-
-	}
-
-	@Override
-	protected void onStart() {
-		super.onStart();
-
-		if (Utils.isCmwap(this))
-			serviceLevel = Utils.getServiceLevel(this);
-		else
-			serviceLevel = WrapService.SERVER_LEVEL_STOP;
-
-		Logger.d(TAG, "服务级别为：" + serviceLevel);
-
-		redrawButton();
-
-	}
-
-	public void onClick(View v) {
-
-		Intent serviceIn = new Intent(this, WrapService.class);
-
-		int message = R.string.serviceTagUp;
-
-		switch (v.getId()) {
-
-		case R.id.Switch:
-
-			if (serviceLevel != WrapService.SERVER_LEVEL_NULL) {
-				stopService(serviceIn);
-				Logger.i(TAG, "禁用服务");
-				Utils.rootCMD(getString(R.string.CMDiptablesDisable));
-				Toast.makeText(this, R.string.serviceTagDown,
-						Toast.LENGTH_SHORT).show();
-				serviceLevel = WrapService.SERVER_LEVEL_NULL;
-				redrawButton();
-				return;
-			} else {
-				serviceLevel = WrapService.SERVER_LEVEL_BASE;
-			}
-			break;
-
-		case R.id.BaseService:
-			if (serviceLevel == WrapService.SERVER_LEVEL_BASE) {
-				serviceLevel = WrapService.SERVER_LEVEL_APPS;
-				message = R.string.serviceTagApp;
-			} else {
-				serviceLevel = WrapService.SERVER_LEVEL_BASE;
-			}
-
-			break;
-		}
-
-		serviceIn.putExtra("SERVERLEVEL", serviceLevel);
-		Logger.i(TAG, "启用服务");
-		startService(serviceIn);
-		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-		redrawButton();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		boolean result = super.onCreateOptionsMenu(menu);
-
-		menu.add(R.string.MENU_TEST);
-
-		return result;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * 判断程序安装状态
-	 * 
-	 * @return -1 初次安装 0 升级安装 1 同版本再次安装
-	 */
-	private int appStatus() {
-		int firsTime = -1;
-		// 判断状态
-		try {
-			FileInputStream in = openFileInput("Version");
-
-			int ver = in.read();
-
-			if (ver == Integer.parseInt(getString(R.string.Version))) {
-				firsTime = 1;
-			} else {
-				firsTime = 0;
-				tag();
-			}
-			in.close();
-		} catch (FileNotFoundException e) {
-			Logger.e(TAG, "No Version File, First installed");
-			tag();
-		} catch (IOException e) {
-			Logger.e(TAG, "IOerror");
-		}
-		return firsTime;
-	}
-
-	/**
-	 * 更新版本号
-	 */
-	private void tag() {
-		FileOutputStream out;
-		try {
-			out = openFileOutput("Version", MODE_PRIVATE);
-
-			out.write(Integer.parseInt(getString(R.string.Version)));
-			out.close();
-		} catch (FileNotFoundException ex) {
-			Logger.e(TAG, "No Version File, First installed");
-		} catch (NumberFormatException ex) {
-			Logger.e(TAG, "版本号解析失败");
-		} catch (IOException ex) {
-			Logger.e(TAG, "写入版本号失败");
-		}
-	}
+	private final int DIALOG_ABOUT_ID = 0;
 
 	/**
 	 * 安装文件
@@ -232,6 +89,144 @@ public class Cmwrap extends Activity implements OnClickListener {
 		return result;
 	}
 
+	public void onClick(View v) {
+
+		Intent serviceIn = new Intent(this, WrapService.class);
+
+		int message = R.string.serviceTagUp;
+
+		switch (v.getId()) {
+
+		case R.id.Switch:
+
+			if (serviceLevel != WrapService.SERVER_LEVEL_NULL) {
+				stopService(serviceIn);
+				Logger.i(TAG, "禁用服务");
+				Utils.rootCMD(getString(R.string.CMDiptablesDisable));
+				Toast.makeText(this, R.string.serviceTagDown,
+						Toast.LENGTH_SHORT).show();
+				serviceLevel = WrapService.SERVER_LEVEL_NULL;
+				redrawButton();
+				return;
+			} else {
+				serviceLevel = WrapService.SERVER_LEVEL_BASE;
+			}
+			break;
+
+		case R.id.BaseService:
+			if (serviceLevel == WrapService.SERVER_LEVEL_BASE) {
+				serviceLevel = WrapService.SERVER_LEVEL_APPS;
+				message = R.string.serviceTagApp;
+			} else {
+				serviceLevel = WrapService.SERVER_LEVEL_BASE;
+			}
+
+			break;
+		}
+
+		serviceIn.putExtra("SERVERLEVEL", serviceLevel);
+		Logger.i(TAG, "启用服务");
+		startService(serviceIn);
+		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+		redrawButton();
+	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		Dialog dialog = null;
+		switch (id) {
+		case DIALOG_ABOUT_ID:
+			dialog = new Dialog(this);
+			dialog.setContentView(R.layout.about);
+			dialog.setTitle(getString(R.string.MENU_ABOUT));
+			break;
+
+		}
+		return dialog;
+	}
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main);
+
+		logWindow = (TextView) findViewById(R.id.logwindow);
+
+		// 判断是否需要更新hosts文件
+		if (appStatus() == 1) {
+			logWindow.append("hosts文件不须更新\n");
+		} else {
+
+			logWindow.append("hosts文件更新...\n");
+			int result = Utils.rootCMD(getString(R.string.CMDremount));
+			if (result != 0) {
+				logWindow.append(getString(R.string.ERR_NO_ROOT));
+			} else {
+				installFiles("/system/etc/hosts", R.raw.hosts, null);
+				logWindow.append("更新完毕。\n");
+			}
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.options_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+		case R.id.TEST:
+
+			return true;
+		case R.id.SETTING:
+
+			return true;
+		case R.id.ABOUT:
+			showDialog(DIALOG_ABOUT_ID);
+			return true;
+
+		}
+		return false;
+	}
+
+	/**
+	 * 判断程序安装状态
+	 * 
+	 * @return -1 初次安装 0 升级安装 1 同版本再次安装
+	 */
+	private int appStatus() {
+		int firsTime = -1;
+		// 判断状态
+		try {
+			FileInputStream in = openFileInput("Version");
+
+			int ver = in.read();
+
+			if (ver == Integer.parseInt(getString(R.string.Version))) {
+				firsTime = 1;
+			} else {
+				firsTime = 0;
+				tag();
+			}
+			in.close();
+		} catch (FileNotFoundException e) {
+			Logger.e(TAG, "No Version File, First installed");
+			tag();
+		} catch (IOException e) {
+			Logger.e(TAG, "IOerror");
+		}
+		return firsTime;
+	}
+
 	private void redrawButton() {
 
 		ToggleButton switcher = (ToggleButton) findViewById(R.id.Switch);
@@ -265,4 +260,39 @@ public class Cmwrap extends Activity implements OnClickListener {
 			switcher.setEnabled(false);
 		}
 	}
+
+	/**
+	 * 更新版本号
+	 */
+	private void tag() {
+		FileOutputStream out;
+		try {
+			out = openFileOutput("Version", MODE_PRIVATE);
+
+			out.write(Integer.parseInt(getString(R.string.Version)));
+			out.close();
+		} catch (FileNotFoundException ex) {
+			Logger.e(TAG, "No Version File, First installed");
+		} catch (NumberFormatException ex) {
+			Logger.e(TAG, "版本号解析失败");
+		} catch (IOException ex) {
+			Logger.e(TAG, "写入版本号失败");
+		}
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+
+		if (Utils.isCmwap(this))
+			serviceLevel = Utils.getServiceLevel(this);
+		else
+			serviceLevel = WrapService.SERVER_LEVEL_STOP;
+
+		Logger.d(TAG, "服务级别为：" + serviceLevel);
+
+		redrawButton();
+
+	}
+
 }
