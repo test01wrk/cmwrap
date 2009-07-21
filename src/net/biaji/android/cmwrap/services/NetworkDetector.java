965e1f66937a013f461ecb4bc8226597d9fdfefa
biaji.cn <biaji.cn@76e3dd0e-3f09-11de-917f-4137a9cc17b9>
2009-07-21 14:16:22 +0000

    1、去除iptables二进制文件 2、加入非中文预支持 3、修改部分hosts项目 4、自动探知改变调整缓冲时间为2分钟 5、将自动感知选项依赖于cmwap专用
    git-svn-id: https://cmwrap.googlecode.com/svn/trunk@113 76e3dd0e-3f09-11de-917f-4137a9cc17b9

--- src/net/biaji/android/cmwrap/services/NetworkDetector.java
+++ src/net/biaji/android/cmwrap/services/NetworkDetector.java
@@ -21,7 +21,7 @@ public class NetworkDetector extends BroadcastReceiver {
 	/**
 	 * 时间间隔，短于此间隔的变化不予实施
 	 */
-	private final long INTERVAL = 1000 * 30;
+	private final long INTERVAL = 1000 * 60 * 2;
 
 	private static int inArray = 0;
 
