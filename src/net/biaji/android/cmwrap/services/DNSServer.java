180a0dcb34b6e73a58dba9b24c4b0a21df537812
biaji.cn <biaji.cn@76e3dd0e-3f09-11de-917f-4137a9cc17b9>
2009-12-20 06:26:25 +0000

    git-svn-id: https://cmwrap.googlecode.com/svn/trunk@180 76e3dd0e-3f09-11de-917f-4137a9cc17b9

--- src/net/biaji/android/cmwrap/services/DNSServer.java
+++ src/net/biaji/android/cmwrap/services/DNSServer.java
@@ -1,10 +1,15 @@
 package net.biaji.android.cmwrap.services;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 
 import net.biaji.android.cmwrap.Logger;
 
@@ -45,26 +50,86 @@ public class DNSServer extends WrapServer {
 
 	@Override
 	public void run() {
-		Socket innerSocket;
+
 		byte[] qbuffer = new byte[576];
 		DatagramPacket dnsq = new DatagramPacket(qbuffer, qbuffer.length);
 		while (true) {
 			try {
-				
+
 				srvSocket.receive(dnsq);
 				byte[] dnsquest = dnsq.getData();
-				
-				//连接外部DNS进行解析。
-				new Thread(){
+
+				// 连接外部DNS进行解析。
+				new Thread() {
+					Socket innerSocket;
+					private final String UA = "biAji's wap channel";
+					private long starTime = System.currentTimeMillis();
+
 					public void run() {
 						DatagramPacket dnsAnswer = null;
+						byte[] buff = new byte[1024];
 						try {
+							buildProxy();
+							if(innerSocket.isConnected()){
+								
+							}
 							srvSocket.send(dnsAnswer);
 						} catch (IOException e) {
 							Logger.e(TAG, "返回DNS解析结果错误", e);
 						}
 					}
-					
+
+					private void buildProxy() {
+
+						Logger.v(TAG, "建立通道");
+						BufferedReader din = null;
+						BufferedWriter dout = null;
+
+						try {
+							innerSocket = new Socket(proxyHost, proxyPort);
+							innerSocket.setKeepAlive(true);
+							innerSocket.setSoTimeout(120 * 1000);
+
+							din = new BufferedReader(new InputStreamReader(
+									innerSocket.getInputStream()));
+							dout = new BufferedWriter(new OutputStreamWriter(
+									innerSocket.getOutputStream()));
+
+							String connectStr = "CONNECT " + proxyHost + ":"
+									+ proxyPort + " HTTP/1.0\r\nUser-agent: "
+									+ this.UA + "\r\n\r\n";
+
+							dout.write(connectStr);
+							dout.flush();
+							Logger.v(TAG, connectStr);
+
+							String result = din.readLine();
+							String line = "";
+							while ((line = din.readLine()) != null) {
+								if (line.trim().equals(""))
+									break;
+								Logger.v(TAG, line);
+							}
+
+							if (result != null && result.contains("200")) {
+								Logger.v(TAG, result);
+								Logger
+										.i(
+												TAG,
+												"通道建立成功， 耗时："
+														+ (System
+																.currentTimeMillis() - starTime)
+														/ 1000);
+							}
+
+						} catch (UnknownHostException e) {
+							Logger.e(TAG, "无法解析代理服务器地址："
+									+ e.getLocalizedMessage());
+						} catch (IOException e) {
+							Logger.e(TAG, "建立隧道失败：" + e.getLocalizedMessage());
+						}
+					}
+
 				}.start();
 
 			} catch (IOException e) {
