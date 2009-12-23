ef42044f86ed13f17c6e992f52f80ecb39946219
biaji.cn <biaji.cn@76e3dd0e-3f09-11de-917f-4137a9cc17b9>
2009-12-23 08:58:01 +0000

    证实可以在dnsmasq的辅助下正常正向解析域名
    git-svn-id: https://cmwrap.googlecode.com/svn/trunk@182 76e3dd0e-3f09-11de-917f-4137a9cc17b9

--- src/net/biaji/android/cmwrap/services/DNSServer.java
+++ src/net/biaji/android/cmwrap/services/DNSServer.java
@@ -2,6 +2,9 @@ package net.biaji.android.cmwrap.services;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -26,6 +29,8 @@ public class DNSServer extends WrapServer {
 	private final String TAG = "CMWRAP->DNSServer";
 
 	private boolean inService = false;
+
+	DatagramPacket dnsq;
 
 	public DNSServer(String name, int port) {
 		this(name, port, "10.0.0.172", 80);
@@ -52,31 +57,64 @@ public class DNSServer extends WrapServer {
 	public void run() {
 
 		byte[] qbuffer = new byte[576];
-		DatagramPacket dnsq = new DatagramPacket(qbuffer, qbuffer.length);
+		dnsq = new DatagramPacket(qbuffer, qbuffer.length);
 		while (true) {
 			try {
 
 				srvSocket.receive(dnsq);
-				byte[] dnsquest = dnsq.getData();
 
 				// 连接外部DNS进行解析。
 				new Thread() {
 					Socket innerSocket;
 					private final String UA = "biAji's wap channel";
 					private long starTime = System.currentTimeMillis();
-					private String target = "4.3.2.1";  //TODO 读取配置
+					private String target = "4.3.2.1:53"; // TODO 读取配置
 
 					public void run() {
-						DatagramPacket dnsAnswer = null;
-						byte[] buff = new byte[1024];
+						DataInputStream in;
+						DataOutputStream out;
 						try {
 							buildProxy();
-							if(innerSocket.isConnected()){
-								
+							if (innerSocket.isConnected()) {
+								// 构造TCP DNS包
+								int dnsqLength = dnsq.getLength();
+								byte[] tcpdnsq = new byte[dnsqLength + 2];
+								System.arraycopy(int2byte(dnsqLength), 0,
+										tcpdnsq, 1, 1);
+								System.arraycopy(dnsq.getData(), 0, tcpdnsq, 2,
+										dnsq.getLength());
+
+								// 转发DNS
+								in = new DataInputStream(innerSocket
+										.getInputStream());
+								out = new DataOutputStream(innerSocket
+										.getOutputStream());
+								out.write(tcpdnsq);
+								out.flush();
+
+								ByteArrayOutputStream bout = new ByteArrayOutputStream();
+
+								int b = -1;
+								while ((b = in.read()) != -1) {
+									bout.write(b);
+								}
+								byte[] response = bout.toByteArray();
+								Logger.i(TAG, "response Length: "
+										+ response.length);
+								DatagramPacket resp = new DatagramPacket(
+										response, 2, response.length - 2);
+								resp.setPort(dnsq.getPort());
+								resp.setAddress(dnsq.getAddress());
+
+								Logger.i(TAG, "Address: " + dnsq.getAddress()
+										+ "port: " + dnsq.getPort() + "SocketAddress: " + dnsq.getAddress());
+								srvSocket.send(resp);
+								innerSocket.close();
 							}
-							srvSocket.send(dnsAnswer);
 						} catch (IOException e) {
 							Logger.e(TAG, "返回DNS解析结果错误", e);
+						} catch (Exception e) {
+							Logger.e(TAG, "解析过程出现其它错误", e);
 						}
 					}
 
@@ -96,8 +134,9 @@ public class DNSServer extends WrapServer {
 							dout = new BufferedWriter(new OutputStreamWriter(
 									innerSocket.getOutputStream()));
 
-							String connectStr = "CONNECT " + target + " HTTP/1.0\r\nUser-agent: "
-									+ this.UA + "\r\n\r\n";
+							String connectStr = "CONNECT " + target
+									+ " HTTP/1.0\r\nUser-agent: " + this.UA
+									+ "\r\n\r\n";
 
 							dout.write(connectStr);
 							dout.flush();
@@ -128,6 +167,16 @@ public class DNSServer extends WrapServer {
 						} catch (IOException e) {
 							Logger.e(TAG, "建立隧道失败：" + e.getLocalizedMessage());
 						}
+					}
+
+					private byte[] int2byte(int res) {
+						byte[] targets = new byte[4];
+
+						targets[0] = (byte) (res & 0xff);// 最低位
+						targets[1] = (byte) ((res >> 8) & 0xff);// 次低位
+						targets[2] = (byte) ((res >> 16) & 0xff);// 次高位
+						targets[3] = (byte) (res >>> 24);// 最高位,无符号右移。
+						return targets;
 					}
 
 				}.start();
