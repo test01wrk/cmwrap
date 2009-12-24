ef506a9cd3da347effeff9f0f777efe87b516ca6
biaji.cn <biaji.cn@76e3dd0e-3f09-11de-917f-4137a9cc17b9>
2009-12-24 09:45:39 +0000

    改为单线程，修正多线程读取长度错误
    git-svn-id: https://cmwrap.googlecode.com/svn/trunk@184 76e3dd0e-3f09-11de-917f-4137a9cc17b9

--- src/net/biaji/android/cmwrap/services/DNSServer.java
+++ src/net/biaji/android/cmwrap/services/DNSServer.java
@@ -32,6 +32,11 @@ public class DNSServer extends WrapServer {
 
 	DatagramPacket dnsq;
 
+	Socket innerSocket;
+	private final String UA = "biAji's wap channel";
+	private long starTime = System.currentTimeMillis();
+	private String target = "4.3.2.1:53"; // TODO 读取配置
+
 	public DNSServer(String name, int port) {
 		this(name, port, "10.0.0.172", 80);
 	}
@@ -61,126 +66,60 @@ public class DNSServer extends WrapServer {
 		while (true) {
 			try {
 
-				srvSocket.receive(dnsq);
+				srvSocket.receive(dnsq); // TODO 解决侦听服务端口出错
 
 				// 连接外部DNS进行解析。
-				new Thread() {
-					Socket innerSocket;
-					private final String UA = "biAji's wap channel";
-					private long starTime = System.currentTimeMillis();
-					private String target = "4.3.2.1:53"; // TODO 读取配置
 
-					public void run() {
-						DataInputStream in;
-						DataOutputStream out;
-						try {
-							buildProxy();
-							if (innerSocket.isConnected()) {
-								// 构造TCP DNS包
-								int dnsqLength = dnsq.getLength();
-								byte[] tcpdnsq = new byte[dnsqLength + 2];
-								System.arraycopy(int2byte(dnsqLength), 0,
-										tcpdnsq, 1, 1);
-								System.arraycopy(dnsq.getData(), 0, tcpdnsq, 2,
-										dnsq.getLength());
+				DataInputStream in;
+				DataOutputStream out;
+				try {
+					buildProxy();
+					if (innerSocket.isConnected()) {
+						// 构造TCP DNS包
+						byte[] data = dnsq.getData();
+						int dnsqLength = dnsq.getLength();
 
-								// 转发DNS
-								in = new DataInputStream(innerSocket
-										.getInputStream());
-								out = new DataOutputStream(innerSocket
-										.getOutputStream());
-								out.write(tcpdnsq);
-								out.flush();
+						Logger.d(TAG, "UDP DNS REQUEST LENGTH: " + dnsqLength);
+						byte[] tcpdnsq = new byte[dnsqLength + 2];
+						System
+								.arraycopy(int2byte(dnsqLength), 0, tcpdnsq, 1,
+										1);
+						System.arraycopy(data, 0, tcpdnsq, 2, dnsqLength);
 
-								ByteArrayOutputStream bout = new ByteArrayOutputStream();
+						// 转发DNS
+						in = new DataInputStream(innerSocket.getInputStream());
+						out = new DataOutputStream(innerSocket
+								.getOutputStream());
+						out.write(tcpdnsq);
+						out.flush();
 
-								int b = -1;
-								while ((b = in.read()) != -1) {
-									bout.write(b);
-								}
-								byte[] response = bout.toByteArray();
-								Logger.d(TAG, "response Length: "
-										+ response.length);
-								DatagramPacket resp = new DatagramPacket(
-										response, 2, response.length - 2);
-								resp.setPort(dnsq.getPort());
-								resp.setAddress(dnsq.getAddress());
+						ByteArrayOutputStream bout = new ByteArrayOutputStream();
 
-								Logger.d(TAG, "Address: " + dnsq.getAddress()
-										+ "port: " + dnsq.getPort() + "SocketAddress: " + dnsq.getAddress());
-								srvSocket.send(resp);
-								innerSocket.close();
-							}
-						} catch (IOException e) {
-							Logger.e(TAG, "返回DNS解析结果错误", e);
-						} catch (Exception e) {
-							Logger.e(TAG, "解析过程出现其它错误", e);
+						int b = -1;
+						while ((b = in.read()) != -1) {
+							bout.write(b);
+						}
+						byte[] response = bout.toByteArray();
+						if (response == null || response.length == 0) {
+							Logger.e(TAG, "返回DNS包长为0");
+						} else {
+							DatagramPacket resp = new DatagramPacket(response,
+									2, response.length - 2);
+							resp.setPort(dnsq.getPort());
+							resp.setAddress(dnsq.getAddress());
+
+							Logger.d(TAG, "正确返回DNS解析，长度：" + resp.getLength());
+							srvSocket.send(resp);
+							innerSocket.close();
 						}
 					}
+				} catch (IOException e) {
+					Logger.e(TAG, "返回DNS解析结果错误", e);
+				}
 
-					private void buildProxy() {
-
-						Logger.v(TAG, "建立通道");
-						BufferedReader din = null;
-						BufferedWriter dout = null;
-
-						try {
-							innerSocket = new Socket(proxyHost, proxyPort);
-							innerSocket.setKeepAlive(true);
-							innerSocket.setSoTimeout(120 * 1000);
-
-							din = new BufferedReader(new InputStreamReader(
-									innerSocket.getInputStream()));
-							dout = new BufferedWriter(new OutputStreamWriter(
-									innerSocket.getOutputStream()));
-
-							String connectStr = "CONNECT " + target
-									+ " HTTP/1.0\r\nUser-agent: " + this.UA
-									+ "\r\n\r\n";
-
-							dout.write(connectStr);
-							dout.flush();
-							Logger.v(TAG, connectStr);
-
-							String result = din.readLine();
-							String line = "";
-							while ((line = din.readLine()) != null) {
-								if (line.trim().equals(""))
-									break;
-								Logger.v(TAG, line);
-							}
-
-							if (result != null && result.contains("200")) {
-								Logger.v(TAG, result);
-								Logger
-										.i(
-												TAG,
-												"通道建立成功， 耗时："
-														+ (System
-																.currentTimeMillis() - starTime)
-														/ 1000);
-							}
-
-						} catch (UnknownHostException e) {
-							Logger.e(TAG, "无法解析代理服务器地址："
-									+ e.getLocalizedMessage());
-						} catch (IOException e) {
-							Logger.e(TAG, "建立隧道失败：" + e.getLocalizedMessage());
-						}
-					}
-
-					private byte[] int2byte(int res) {
-						byte[] targets = new byte[4];
-
-						targets[0] = (byte) (res & 0xff);// 最低位
-						targets[1] = (byte) ((res >> 8) & 0xff);// 次低位
-						targets[2] = (byte) ((res >> 16) & 0xff);// 次高位
-						targets[3] = (byte) (res >>> 24);// 最高位,无符号右移。
-						return targets;
-					}
-
-				}.start();
-
+			} catch (SocketException e) {
+				Logger.e(TAG, "", e);
+				break;
 			} catch (IOException e) {
 				Logger.e(TAG, "", e);
 			}
@@ -188,10 +127,63 @@ public class DNSServer extends WrapServer {
 
 	}
 
+	private void buildProxy() {
+		starTime = System.currentTimeMillis();
+		Logger.v(TAG, "建立通道");
+		BufferedReader din = null;
+		BufferedWriter dout = null;
+
+		try {
+			innerSocket = new Socket(proxyHost, proxyPort);
+			innerSocket.setKeepAlive(true);
+			innerSocket.setSoTimeout(120 * 1000);
+
+			din = new BufferedReader(new InputStreamReader(innerSocket
+					.getInputStream()));
+			dout = new BufferedWriter(new OutputStreamWriter(innerSocket
+					.getOutputStream()));
+
+			String connectStr = "CONNECT " + target
+					+ " HTTP/1.0\r\nUser-agent: " + this.UA + "\r\n\r\n";
+
+			dout.write(connectStr);
+			dout.flush();
+			Logger.v(TAG, connectStr);
+
+			String result = din.readLine();
+			String line = "";
+			while ((line = din.readLine()) != null) {
+				if (line.trim().equals(""))
+					break;
+				Logger.v(TAG, line);
+			}
+
+			if (result != null && result.contains("200")) {
+				Logger.v(TAG, result);
+				Logger.i(TAG, "通道建立成功， 耗时："
+						+ (System.currentTimeMillis() - starTime) / 1000);
+			}
+
+		} catch (IOException e) {
+			Logger.e(TAG, "建立隧道失败：" + e.getLocalizedMessage());
+		}
+	}
+
+	private byte[] int2byte(int res) {
+		byte[] targets = new byte[4];
+
+		targets[0] = (byte) (res & 0xff);// 最低位
+		targets[1] = (byte) ((res >> 8) & 0xff);// 次低位
+		targets[2] = (byte) ((res >> 16) & 0xff);// 次高位
+		targets[3] = (byte) (res >>> 24);// 最高位,无符号右移。
+		return targets;
+	}
+
 	@Override
 	public void close() throws IOException {
 		inService = false;
 		srvSocket.close();
+		Logger.i(TAG, "服务关闭");
 	}
 
 	@Override
